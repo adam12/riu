9d8da86068bab8ede7b7a3af9a8bc88d326a9154
Adam Daniels <adam@mediadrive.ca>
2024-01-26 21:43:40 -0500

    Renders docs

--- lib/riu.rb
+++ lib/riu.rb
@@ -1,6 +1,30 @@
 require "reline"
+require "rdoc"
 
+# @ri.list_methods_matching("parse")
 module Riu
+  class Visitor
+    def accept_heading(v)
+      puts "= #{v.text}"
+    end
+
+    def accept_blank_line(v)
+      puts
+    end
+
+    def accept_paragraph(v)
+      puts v.text
+    end
+
+    def accept_rule(v)
+      puts "=" * 80
+    end
+
+    def accept_verbatim(v)
+      puts v.text
+    end
+  end
+
   class CLI
     attr_accessor :argv
     attr_accessor :prompt
@@ -10,9 +34,11 @@ module Riu
       @argv = argv
       @prompt = "riu> "
       @use_history = true
+      @ri = RDoc::RI::Driver.new
     end
 
     def run
+      # require "debug"; debugger
       loop do
         text = Reline.readline(prompt, use_history)
         case text
@@ -20,9 +46,20 @@ module Riu
           break
         when ""
           puts "Use C-d to exit"
-        end
+        else
+          name = @ri.expand_name(text)
+          filtered = @ri.lookup_method(name)
 
-        p text
+          method_out = @ri.method_document("Zlib.crc32", filtered)
+          visitor = Visitor.new
+          method_out.parts.each { |part| part.accept(visitor) }
+          # @ri.find_methods(name) do |store, klass, ancestor, types, method|
+          #   p "Klass: #{klass} #{method}"
+          #   p TYPES: types
+          # end
+        end
+      rescue RDoc::RI::Driver::NotFoundError
+        puts "Not found"
       end
     rescue Interrupt
       puts "^C"
